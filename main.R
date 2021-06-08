ac988097d6cd8a97c1b87ce55f6348dfe0a9d605
ginberg <info@gerinberg.com>
2021-06-08 07:47:58 +0000

    remove shift_offset reshape -> reshape2

--- main.R
+++ main.R
@@ -1,21 +1,19 @@
 library(tercen)
 library(dplyr)
-library(reshape)
+library(reshape2)
 
-log.cutoff <- function(df, treatment_negative_values, shift_offset, cut_off, log_base) {
-  count = cast(df, .ri~.ci, value = ".y", fun.aggregate = length)
-  if (any(count[,-1] > 1)) {
+log.cutoff <- function(df, treatment_negative_values, cut_off, log_base) {
+  count = acast(df, .ri~.ci, fun.aggregate = length, value.var = ".y")
+  if (any(count > 1)){
     stop("Error: multiple values per technical replication have been found.")
   }
   
   if (treatment_negative_values == "None"){
-    shift_offset <- 0
-    cut_off      <- 0
+    cut_off <- 0
   }
   
   if (log_base >= 0){
-    df$.y <- df$.y + shift_offset
-    bCut  <- df$.y < cut_off
+    bCut <- df$.y < cut_off
     df$.y[bCut] = cut_off
     result = data.frame(.ri = df$.ri, .ci = df$.ci, logTransformed = log(df$.y, log_base))
   } else {
@@ -27,12 +25,11 @@ log.cutoff <- function(df, treatment_negative_values, shift_offset, cut_off, log
 ctx = tercenCtx()
 
 treatment_negative_values <- ifelse(is.null(ctx$op.value('Treatment of negative values')), 'Mask at cut-off', ctx$op.value('Treatment of negative values'))
-shift_offset              <- ifelse(is.null(ctx$op.value('shift offset')), 1, as.double(ctx$op.value('shift offset')))
 cut_off                   <- ifelse(is.null(ctx$op.value('cut-off')), 1, as.double(ctx$op.value('cut-off')))
 log_base                  <- ifelse(is.null(ctx$op.value('logBase')), 2, as.double(ctx$op.value('logBase')))
 
 ctx %>% 
   select(.ci, .ri, .y) %>%
-  log.cutoff(., treatment_negative_values, shift_offset, cut_off, log_base) %>%
+  log.cutoff(., treatment_negative_values, cut_off, log_base) %>%
   ctx$addNamespace() %>%
   ctx$save()
